46b4b7c1ce716f4f90c9a4c6108dc70e8169dad9
Emam Saimon <likonsaimon123@gmail.com>
2025-06-13 09:03:12 +0600

    Update q17.py

--- OOPs in python/q17.py
+++ OOPs in python/q17.py
@@ -3,3 +3,8 @@ print("hello")
 print("My name is Emam Saimon. IIUC ")
 print("MySQL, PostgreSQL,  Docker🐋")
 print(" how are you baby")
+
+print("hello")
+print("My name is Emam Saimon. IIUC ")
+print("MySQL, PostgreSQL,  Docker🐋")
+print(" how are you baby")
