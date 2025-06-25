91ef450cc11fd4ef96dfb1f49da8418887c7ce1c
Emam Saimon <likonsaimon123@gmail.com>
2025-06-26 00:24:53 +0600

    Update q17.py

--- OOPs in python/q17.py
+++ OOPs in python/q17.py
@@ -8,4 +8,5 @@ if x < 5 or x > 10:
 if not (x == 5):
     print("x is not equal to 5")
 
-print("Ishowspeed is awesome  mrbeast")
+print("Ishowspeed is awesome mrbeast")
+print("PowerColor AMD Radeon RX 7700 XT 12GB GDDR6 Graphics Card")
