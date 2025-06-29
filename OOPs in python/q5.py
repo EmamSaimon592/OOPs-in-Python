b715f2e3f7518be21f689fc4232e392d50d3c1b2
Emam Saimon <likonsaimon123@gmail.com>
2025-06-29 18:23:08 +0600

    Update q5.py

--- OOPs in python/q5.py
+++ OOPs in python/q5.py
@@ -1,7 +1,7 @@
 # Write a program to print the area of a rectangle by creating a class named 'Area' having 
 # two functions. First function named as 'setDim' takes the length and breadth of the 
 # rectangle as parameters and the second function named as 'getArea' returns the area of 
-# the rectangle. Length and breadth of the rectangle are entered through keyboard. 
+# the rectangle. Length and breadth of the rectangle are entered through keyboard. --------
 
 class Area:
     def __init__(self):
