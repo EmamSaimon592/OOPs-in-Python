2cd1bb1741df6fba46ffe42dc19f3993208972f9
Emam Saimon <likonsaimon123@gmail.com>
2025-07-03 20:28:32 +0600

    Update q1.py

--- OOPs in python/q1.py
+++ OOPs in python/q1.py
@@ -1,4 +1,4 @@
-# Create a class named 'Student' with a string variable 'name' and an integer variable 
+# ----Create a class named 'Student' with a string variable 'name' and an integer variable 
 # 'roll_no'. Assign the value of roll_no as '2' and that of name as "Jamshed" by creating 
 # an object of the class Student. 
 
