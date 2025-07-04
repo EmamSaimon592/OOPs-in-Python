e4d9d9f8693d9be55075616cab3f1d84d5e7011d
Emam Saimon <likonsaimon123@gmail.com>
2025-07-04 16:55:38 +0600

    Update q7.py

--- OOPs in python/q7.py
+++ OOPs in python/q7.py
@@ -1,4 +1,4 @@
-# Print the sum, difference and product of two complex numbers by creating a class 
+# -----> the sum, difference and product of two complex numbers by creating a class 
 # named 'Complex' with separate functions for each operation whose real and imaginary 
 # parts are entered by the user
 
